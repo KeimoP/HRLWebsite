71490a3fca11dadb13769ea04e52dc2d572db0c7
KeimoP <keimo@hkhk.edu.ee>
2025-04-08 10:20:32 +0300

    update

--- public/blog.php
+++ public/blog.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HRL OÜ - Blogi</title>
-    <link rel="icon" href="/images/logo.png">
+    <link rel="icon" href="./assets/images/logo.png">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
